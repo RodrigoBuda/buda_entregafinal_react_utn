06ca7baed96c097d23f2b79780079925f8eb239b
RodrigoBuda <100789530+RodrigoBuda@users.noreply.github.com>
2023-12-01 01:22:31 -0300

    tercer_commit

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,19 +1,32 @@
-export default function TaskList({ tasks, onDeleteTask, onChangeStatus}) {
-
-const msg = (tetxt) =>alert(text)
-    
+export default function TaskList({ tasks, onDeleteTask, onChangeStatus }) {
+  const msg = (tetxt) => alert(text);
 
   return (
-    <div>
+    <div className="cont-taskList">
       <ul>
         {tasks.map((t) => (
           <li key={t.id}>
-            
-            {t.id.slice(0, 4)} - {t.description} -
-            {t.isCompleted ? "terminado" : "pendiente"}
-            <button className="btn btn-borrar"  onClick={() => onDeleteTask(t.id)}>✖️</button>
-            <button className="btn btn-terminado" onClick={()=>onChangeStatus(t.id)}>✔️</button>
-           
+            <div className="cont-id-tarea">
+              {t.id.slice(0, 4)} ❗ {t.description} ➡️
+            </div>
+
+            <div className="btns-hecho-pend">
+              <div className="hecho-pendiente">
+                {t.isCompleted ? "Hecho 🫡" : "Pendiente 😴"}
+              </div>
+              <button
+                className="btn btn-terminado"
+                onClick={() => onChangeStatus(t.id)}
+              >
+                ✔️
+              </button>
+              <button
+                className="btn btn-borrar"
+                onClick={() => onDeleteTask(t.id)}
+              >
+                ✖️
+              </button>
+            </div>
           </li>
         ))}
       </ul>
